a07580fc9216bb0eedb7f87d56af41d9e7878f3e
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-09-25 11:30:57 -0400

    commit

---
+++ puntoVentaModelo/puntoVentaModelo/entity.Designer.cs
@@ -0,0 +1,2 @@
+﻿// Seeing this comment implies that the code generation for file 'D:\wilmer\developer\github\data\DLR-POS\puntoVentaModelo\puntoVentaModelo\entity.edmx'
+// failed. See the ErrorList for details.
